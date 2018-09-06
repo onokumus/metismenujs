86b317124f1e538078c1d9945886ee5720d3fa3d
onokumus <onokumus@gmail.com>
2018-09-06 11:05:56 +0300

    update version

--- package.js
+++ package.js
@@ -1,6 +1,6 @@
 Package.describe({
   name: 'onokumus:metismenujs',
-  version: '1.0.1',
+  version: '1.0.2',
   summary: 'A menu plugin',
   git: 'https://github.com/onokumus/metismenujs',
   documentation: 'README.md'
