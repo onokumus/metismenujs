22fbc525bbb664b70696cf98a1f5f76d871a8e48
onokumus <onokumus@gmail.com>
2020-04-23 10:29:32 +0300

    refactor metismenu

--- src/index.ts
+++ src/index.ts
@@ -1,232 +1,236 @@
-import { ClassName, Default, MetisMenuEvents } from "./constant";
-import { IMMOptions } from "./interface";
+/* eslint-disable max-len */
+import { ClassName, Default, MetisMenuEvents } from './constant';
+import { IMMOptions } from './interface';
+import { EventType, SpecificEventListener, CustomEventListener } from './types';
+import { closest } from './util';
 
 class MetisMenu {
-  public config: IMMOptions;
-  public element: HTMLElement;
-  private isTransitioning: boolean;
-  private disposed: boolean;
-  private ulArr: any[];
-  private cacheEl: HTMLElement;
-  private cacheConfig: IMMOptions;
-  private listenerOb: any[];
+  protected config: IMMOptions;
+
+  protected element: Element;
+
+  protected isTransitioning!: boolean;
+
+  protected disposed: boolean;
+
+  protected triggerArr: Array<Element>;
 
   /**
    * Creates an instance of MetisMenu.
    *
    * @constructor
-   * @param {HTMLElement | string} element
+   * @param {Element | string} element
    * @param {IMMOptions} [options]
    * @memberof MetisMenu
    */
 
-  constructor(element: HTMLElement | string, options?: IMMOptions) {
-    this.element =
-      typeof element === "string" ? document.querySelector(element) : element;
-    this.cacheEl = this.element;
+  constructor(element: Element | string, options?: IMMOptions) {
+    this.element = MetisMenu.isElement(element) ? element : document.querySelector<HTMLElement>(element)!;
     this.config = { ...Default, ...options };
-    this.cacheConfig = this.config;
     this.disposed = false;
-    this.ulArr = [];
-    this.listenerOb = [];
+    this.triggerArr = [];
     this.init();
   }
 
-  public update() {
-    this.disposed = false;
-    this.element = this.cacheEl;
-    this.config = this.cacheConfig;
-    this.init();
-  }
-
-  public dispose() {
-    for (const lo of this.listenerOb) {
-      for (const key in lo) {
-        if (lo.hasOwnProperty(key)) {
-          const el = lo[key];
-          el[1].removeEventListener(el[0], el[2]);
-        }
-      }
-    }
-    this.ulArr = [];
-    this.listenerOb = [];
-    this.config = null;
-    this.element = null;
-    this.disposed = true;
-  }
-
-  public on(event: MetisMenuEvents, fn: EventListenerOrEventListenerObject) {
-    this.element.addEventListener(event, fn, false);
-    return this;
-  }
-
-  public off(event: MetisMenuEvents, fn: EventListenerOrEventListenerObject) {
-    this.element.removeEventListener(event, fn);
-    return this;
-  }
-
-  private emit(
-    event: MetisMenuEvents,
-    eventDetail: object,
-    shouldBubble = false
-  ) {
-    let evt;
-    if (typeof CustomEvent === "function") {
-      evt = new CustomEvent(event, {
-        bubbles: shouldBubble,
-        detail: eventDetail
-      });
-    } else {
-      evt = document.createEvent("CustomEvent");
-      evt.initCustomEvent(event, shouldBubble, false, eventDetail);
-    }
-    this.element.dispatchEvent(evt);
-    return this;
+  static attach(el: Element, opt?: IMMOptions) {
+    return new MetisMenu(el, opt);
   }
 
   private init(): void {
-    this.element.classList.add(ClassName.METIS);
-    this.ulArr = [].slice.call(
-      this.element.querySelectorAll(this.config.subMenu)
-    );
-    for (const ul of this.ulArr) {
-      const li = ul.parentNode;
-      ul.classList.add(ClassName.COLLAPSE);
+    const { METIS, ACTIVE, COLLAPSE } = ClassName;
+    this.element.classList.add(METIS);
 
-      if (li.classList.contains(ClassName.ACTIVE)) {
-        this.show(ul);
+    [].slice.call(this.element.querySelectorAll<HTMLElement>(this.config.subMenu!)).forEach((ul: Element) => {
+      ul.classList.add(COLLAPSE!);
+      const li = closest(ul, this.config.parentTrigger!);
+
+      if (li?.classList.contains(ACTIVE!)) {
+        this.show(ul as HTMLElement);
       } else {
         this.hide(ul);
       }
-      const a = li.querySelector(this.config.triggerElement);
-      if (a.getAttribute("aria-disabled") === "true") {
+
+      const a = li?.querySelector<HTMLElement>(this.config.triggerElement!);
+      if (a?.getAttribute('aria-disabled') === 'true') {
         return;
       }
 
-      a.setAttribute("aria-expanded", "false");
-      const listenerOb = {
-        aClick: ["click", a, this.clickEvent.bind(this)]
-      };
+      a?.setAttribute('aria-expanded', 'false');
 
-      for (const key in listenerOb) {
-        if (listenerOb.hasOwnProperty(key)) {
-          const listener = listenerOb[key];
-          listener[1].addEventListener(listener[0], listener[2]);
-        }
-      }
-      this.listenerOb.push(listenerOb);
-    }
+      a?.addEventListener('click', this.clickEvent.bind(this));
+      this.triggerArr.push(a!);
+    });
   }
 
-  private clickEvent(ev?) {
+  private clickEvent(evt?: Event) {
     if (!this.disposed) {
-      if (ev.currentTarget.tagName === "A") {
-        ev.preventDefault();
+      const target = evt?.target as Element | null;
+      if (target && target.tagName === 'A') {
+        evt!.preventDefault();
       }
 
-      const li = ev.currentTarget.parentNode;
-      const ul = li.querySelector(this.config.subMenu);
-      this.toggle(ul);
+      const li = target?.parentNode;
+      const ul = li?.querySelector(this.config.subMenu!);
+      this.toggle(ul!);
     }
   }
 
-  private toggle(ul) {
-    if (ul.parentNode.classList.contains(ClassName.ACTIVE)) {
-      this.hide(ul);
+  update() {
+    this.disposed = false;
+  }
+
+  dispose() {
+    this.triggerArr.forEach((arr) => {
+      arr.removeEventListener('click', this.clickEvent.bind(this));
+    });
+    this.disposed = true;
+  }
+
+  on<K extends EventType>(evtType: K, handler: SpecificEventListener<K>, options?: AddEventListenerOptions | boolean): MetisMenu;
+
+  on<E extends Event>(evtType: E, handler: CustomEventListener<E>, options?: AddEventListenerOptions | boolean): MetisMenu;
+
+  on(evtType: MetisMenuEvents, handler: EventListener, options?: AddEventListenerOptions | boolean) {
+    this.element.addEventListener(evtType, handler, options);
+    return this;
+  }
+
+  off<K extends EventType>(evtType: K, handler: SpecificEventListener<K>, options?: AddEventListenerOptions | boolean): MetisMenu;
+
+  off<E extends Event>(evtType: E, handler: CustomEventListener<E>, options?: AddEventListenerOptions | boolean): MetisMenu;
+
+  off(evtType: MetisMenuEvents, handler: EventListener, options?: AddEventListenerOptions | boolean) {
+    this.element.removeEventListener(evtType, handler, options);
+    return this;
+  }
+
+  emit<T extends object>(evtType: string, evtData: T, shouldBubble = false) {
+    let evt: CustomEvent<T>;
+    if (typeof CustomEvent === 'function') {
+      evt = new CustomEvent<T>(evtType, {
+        bubbles: shouldBubble,
+        detail: evtData,
+      });
     } else {
-      this.show(ul);
+      evt = document.createEvent('CustomEvent');
+      evt.initCustomEvent(evtType, shouldBubble, false, evtData);
+    }
+    this.element.dispatchEvent(evt);
+  }
+
+  toggle(ul: Element) {
+    const li = closest(ul, this.config.parentTrigger!);
+    if (MetisMenu.isElement(li)) {
+      if (li.classList.contains(ClassName.ACTIVE!)) {
+        this.hide(ul);
+      } else {
+        this.show(ul);
+      }
     }
   }
 
-  private show(ul) {
-    if (this.isTransitioning || ul.classList.contains(ClassName.COLLAPSING)) {
+  show(el: Element) {
+    const ul = el as HTMLElement;
+    const {
+      ACTIVE, COLLAPSE, COLLAPSED, COLLAPSING, SHOW,
+    } = ClassName;
+
+    if (this.isTransitioning || ul.classList.contains(COLLAPSING!)) {
       return;
     }
     const complete = () => {
-      ul.classList.remove(ClassName.COLLAPSING);
-      ul.style.height = "";
-      ul.removeEventListener("transitionend", complete);
+      ul.classList.remove(COLLAPSING!);
+      ul.style.height = '';
+      ul.removeEventListener('transitionend', complete);
       this.setTransitioning(false);
-      this.emit("shown.metisMenu", {
-        shownElement: ul
+      this.emit('shown.metisMenu', {
+        shownElement: ul,
       });
     };
 
-    const li = ul.parentNode;
-    li.classList.add(ClassName.ACTIVE);
+    const li = closest(ul, this.config.parentTrigger!);
+    li?.classList.add(ACTIVE!);
 
-    const a = li.querySelector(this.config.triggerElement);
-    a.setAttribute("aria-expanded", "true");
-    a.classList.remove(ClassName.COLLAPSED);
+    const a = li?.querySelector<HTMLElement>(this.config.triggerElement!);
+    a?.setAttribute('aria-expanded', 'true');
+    a?.classList.remove(COLLAPSED!);
 
-    ul.style.height = "0px";
-    ul.classList.remove(ClassName.COLLAPSE);
-    ul.classList.remove(ClassName.SHOW);
-    ul.classList.add(ClassName.COLLAPSING);
+    ul.style.height = '0px';
+    ul.classList.remove(COLLAPSE!);
+    ul.classList.remove(SHOW!);
+    ul.classList.add(COLLAPSING!);
     const eleParentSiblins = [].slice
-      .call(li.parentNode.children)
-      .filter(c => c !== li);
+      .call(li?.parentNode?.children)
+      .filter((c) => c !== li);
     if (this.config.toggle && eleParentSiblins.length > 0) {
-      for (const sibli of eleParentSiblins) {
-        const sibUl = sibli.querySelector(this.config.subMenu);
-        if (sibUl !== null) {
+      eleParentSiblins.forEach((sibli: Element) => {
+        const sibUl = sibli.querySelector<HTMLElement>(this.config.subMenu!);
+        if (sibUl) {
           this.hide(sibUl);
         }
-      }
+      });
     }
 
     this.setTransitioning(true);
 
-    ul.classList.add(ClassName.COLLAPSE);
-    ul.classList.add(ClassName.SHOW);
-    ul.style.height = ul.scrollHeight + "px";
-    this.emit("show.metisMenu", {
-      showElement: ul
+    ul.classList.add(COLLAPSE!);
+    ul.classList.add(SHOW!);
+    ul.style.height = `${ul.scrollHeight}px`;
+    this.emit('show.metisMenu', {
+      showElement: ul,
     });
-    ul.addEventListener("transitionend", complete);
+    ul.addEventListener('transitionend', complete);
   }
 
-  private hide(ul) {
-    if (this.isTransitioning || !ul.classList.contains(ClassName.SHOW)) {
+  hide(el: Element) {
+    const {
+      ACTIVE, COLLAPSE, COLLAPSED, COLLAPSING, SHOW,
+    } = ClassName;
+    const ul = el as HTMLElement;
+    if (this.isTransitioning || !ul.classList.contains(SHOW!)) {
       return;
     }
-    this.emit("hide.metisMenu", {
-      hideElement: ul
+    this.emit('hide.metisMenu', {
+      hideElement: ul,
     });
 
-    const li = ul.parentNode;
-    li.classList.remove(ClassName.ACTIVE);
+    const li = closest(ul, this.config.parentTrigger!);
+    li?.classList.remove(ACTIVE!);
 
     const complete = () => {
-      ul.classList.remove(ClassName.COLLAPSING);
-      ul.classList.add(ClassName.COLLAPSE);
-      ul.style.height = "";
-      ul.removeEventListener("transitionend", complete);
+      ul.classList.remove(COLLAPSING!);
+      ul.classList.add(COLLAPSE!);
+      ul.style.height = '';
+      ul.removeEventListener('transitionend', complete);
       this.setTransitioning(false);
-      this.emit("hidden.metisMenu", {
-        hiddenElement: ul
+      this.emit('hidden.metisMenu', {
+        hiddenElement: ul,
       });
     };
 
-    ul.style.height = ul.getBoundingClientRect().height + "px";
-    ul.style.height = ul.offsetHeight + "px";
+    ul.style.height = `${ul.getBoundingClientRect().height}px`;
+    ul.style.height = `${ul.offsetHeight}px`;
 
-    ul.classList.add(ClassName.COLLAPSING);
-    ul.classList.remove(ClassName.COLLAPSE);
-    ul.classList.remove(ClassName.SHOW);
+    ul.classList.add(COLLAPSING!);
+    ul.classList.remove(COLLAPSE!);
+    ul.classList.remove(SHOW!);
     this.setTransitioning(true);
 
-    ul.addEventListener("transitionend", complete);
+    ul.addEventListener('transitionend', complete);
 
-    ul.style.height = "0px";
+    ul.style.height = '0px';
 
-    const a = li.querySelector(this.config.triggerElement);
-    a.setAttribute("aria-expanded", "false");
-    a.classList.add(ClassName.COLLAPSED);
+    const a = li?.querySelector(this.config.triggerElement!);
+    a?.setAttribute('aria-expanded', 'false');
+    a?.classList.add(COLLAPSED!);
   }
 
-  private setTransitioning(isTransitioning) {
+  private setTransitioning(isTransitioning: boolean) {
     this.isTransitioning = isTransitioning;
+  }
+
+  static isElement(element: unknown): element is Element {
+    return Boolean((element as Element).classList);
   }
 }
 
