49d52b8c3f48eba3991591aa212c9130b938ab82
onokumus <onokumus@gmail.com>
2020-04-23 10:22:28 +0300

    update ClassName

--- src/constant.ts
+++ src/constant.ts
@@ -1,23 +1,23 @@
-import { IMMClassNames, IMMOptions } from "./interface";
+import { IMMOptions } from './interface';
 
 export type MetisMenuEvents =
-  | "show.metisMenu"
-  | "shown.metisMenu"
-  | "hide.metisMenu"
-  | "hidden.metisMenu";
+  | 'show.metisMenu'
+  | 'shown.metisMenu'
+  | 'hide.metisMenu'
+  | 'hidden.metisMenu';
 
 export const Default: IMMOptions = {
-  parentTrigger: "li",
-  subMenu: "ul",
+  parentTrigger: 'li',
+  subMenu: 'ul',
   toggle: true,
-  triggerElement: "a"
+  triggerElement: 'a',
 };
 
-export const ClassName: IMMClassNames = {
-  ACTIVE: "mm-active",
-  COLLAPSE: "mm-collapse",
-  COLLAPSED: "mm-collapsed",
-  COLLAPSING: "mm-collapsing",
-  METIS: "metismenu",
-  SHOW: "mm-show"
+export const ClassName = {
+  ACTIVE: 'mm-active',
+  COLLAPSE: 'mm-collapse',
+  COLLAPSED: 'mm-collapsed',
+  COLLAPSING: 'mm-collapsing',
+  METIS: 'metismenu',
+  SHOW: 'mm-show',
 };
