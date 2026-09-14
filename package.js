80d813d1ab1d7724d804a58dcc917aefb9cafa22
Osman Nuri Okumuş <742498+onokumus@users.noreply.github.com>
2026-09-14 21:42:50 +0300

    chore: format configuration files

--- package.js
+++ package.js
@@ -1,14 +1,11 @@
 Package.describe({
-  name: 'onokumus:metismenujs',
-  version: '1.5.0',
-  summary: 'A menu plugin',
-  git: 'https://github.com/onokumus/metismenujs',
-  documentation: 'README.md'
+	name: "onokumus:metismenujs",
+	version: "1.5.0",
+	summary: "A menu plugin",
+	git: "https://github.com/onokumus/metismenujs",
+	documentation: "README.md",
 });
 
-Package.onUse(function(api) {
-  api.addFiles([
-    "dist/metismenujs.css",
-    "dist/metismenujs.js"
-  ], 'client');
+Package.onUse((api) => {
+	api.addFiles(["dist/metismenujs.css", "dist/metismenujs.js"], "client");
 });
